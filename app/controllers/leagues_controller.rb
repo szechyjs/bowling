b5a27b923e78a2530305ad8c63a0579c8a14e8d1
Jared Szechy <jared.szechy@gmail.com>
2014-01-16 06:51:06 -0500

    Fix group query for pg

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -53,7 +53,7 @@ class LeaguesController < ApplicationController
     @league = League.find(params[:id])
     @teams = @league.teams
     weeks = []
-    @league.series.group(:week).each do |series|
+    @league.series.select(:week).group(:week).each do |series|
       weeks << series.week
     end
     @weeks = weeks
