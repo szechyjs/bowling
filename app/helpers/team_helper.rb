b47cf5d51d8c081d14e135223186a4b50c50099b
Jared Szechy <jared.szechy@gmail.com>
2014-01-21 01:24:42 -0500

    Fix pg requirement for grouping

--- app/helpers/team_helper.rb
+++ app/helpers/team_helper.rb
@@ -1,7 +1,7 @@
 module TeamHelper
   def team_handicap(team, week)
     total = 0
-    series_bowlers = Series.where(team: team, week: week).group(:bowler_id)
+    series_bowlers = Series.where(team: team, week: week).group(:id, :bowler_id)
     series_bowlers.each do |series|
       total += bowler_handicap series.bowler, team, week
     end
