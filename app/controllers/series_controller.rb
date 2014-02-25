b3540900afd79ff2c645848d9a9bbbf4fbf14b5a
Jared Szechy <jared.szechy@gmail.com>
2014-02-25 16:20:05 -0500

    Reverse series index order, newest first

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -6,7 +6,7 @@ class SeriesController < ApplicationController
   # GET /series
   # GET /series.json
   def index
-    @series = Series.includes({:team => :league}, :bowler, :scores)
+    @series = Series.includes({:team => :league}, :bowler, :scores).order("id DESC")
     respond_with(@series)
   end
 
