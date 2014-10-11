5769b2c10274ebf3d5649ee94ad2eed9608d5db6
Jared Szechy <jared.szechy@gmail.com>
2014-10-11 03:37:13 -0400

    Fix session route names.
    This bug may have existed before the Devise update. After getting a 403
    response back from Google OAuth2 devise was redirecting to
    new_session_path, which did not exist.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Bowling::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
 
   devise_scope :user do
-    get 'login', :to => 'users/sessions#new', :as => :new_user_session
-    get 'logout', :to => 'users/sessions#destroy', :as => :destroy_user_session
+    get 'login', :to => 'users/sessions#new', :as => :new_session
+    get 'logout', :to => 'users/sessions#destroy', :as => :destroy_session
   end
 
   resources :leagues do
@@ -19,5 +19,5 @@ Bowling::Application.routes.draw do
     end
   end
 
-  root :to => "home#index"
+  root :to => 'home#index'
 end
